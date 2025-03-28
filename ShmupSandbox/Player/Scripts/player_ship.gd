27965bf9e83f730b902c92b83a450abd3f55162b
rnakkas <rahman.nakkas@gmail.com>
2025-03-29 07:19:54 +1100

    started creating player shooting

--- ShmupSandbox/Player/Scripts/player_ship.gd
+++ ShmupSandbox/Player/Scripts/player_ship.gd
@@ -1,14 +1,30 @@
 extends CharacterBody2D
 
-@export var _speed : float
+## Velocity
+@export var _max_speed : float
 @export var _acceleration : float
 @export var _damping : float
 
+## Sprites
 @onready var body: AnimatedSprite2D = %Body
 @onready var thrusterL : AnimatedSprite2D = %ThrusterL
 @onready var thrusterR : AnimatedSprite2D = %ThrusterR
 @onready var muzzle_flash_l: AnimatedSprite2D = %MuzzleFlashL
 @onready var muzzle_flash_r: AnimatedSprite2D = %MuzzleFlashR
+
+## Muzzle
+@onready var muzzle_l : Marker2D = %MuzzleL
+@onready var muzzle_r : Marker2D = %MuzzleR
+
+## Timers
+@onready var shot_cooldown_timer : Timer = %shot_cooldown_timer
+
+## Packed scenes
+var bullet : PackedScene = preload("res://ShmupSandbox/Player/Scenes/player_bullet.tscn")
+
+var min_bounds : Vector2 = Vector2(0, 0)
+var max_bounds : Vector2 = Vector2(720, 1280)
+var on_shooting_cooldown : bool
 
 func _ready() -> void:
 	body.play("idle")
@@ -17,14 +33,47 @@ func _ready() -> void:
 	muzzle_flash_l.play("idle")
 	muzzle_flash_r.play("idle")
 
-func _physics_process(delta: float) -> void:
+func handle_input(delta) -> void:
+	var input_dir := Vector2.ZERO
 	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * _speed
+	# Get input direction
+	input_dir.x = Input.get_axis("move_left", "move_right")
+	input_dir.y = Input.get_axis("move_up", "move_down")
+	input_dir = input_dir.normalized()  # Normalize for diagonal movement
+	
+	# Acceleration
+	if input_dir != Vector2.ZERO:
+		velocity = velocity.move_toward(input_dir * _max_speed, _acceleration * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, _speed)
+		velocity = velocity.move_toward(Vector2.ZERO, _damping * delta)
 
+func clamp_movement_to_screen_bounds() -> void:
+	# Clamp position within bounds
+	var offset : float = 20.0
+	
+	position.x = clamp(position.x, offset - min_bounds.x, max_bounds.x - offset)
+	position.y = clamp(position.y, offset - min_bounds.y, max_bounds.y - offset)
+
+func handle_shooting() -> void:
+	if Input.is_action_pressed("attack") && !on_shooting_cooldown:
+		on_shooting_cooldown = true
+		shot_cooldown_timer.start()
+		
+		var bullet_instance_l := bullet.instantiate()
+		var bullet_instance_r := bullet.instantiate()
+		bullet_instance_l.position = muzzle_l.global_position
+		bullet_instance_r.position = muzzle_r.global_position
+		get_tree().current_scene.add_child(bullet_instance_l)
+		get_tree().current_scene.add_child(bullet_instance_r)
+
+func _physics_process(delta):
+	handle_input(delta)
 	move_and_slide()
+
+func _process(_delta: float) -> void:
+	clamp_movement_to_screen_bounds()
+	handle_shooting()
+
+## Signal connections
+func _on_shot_cooldown_timer_timeout() -> void:
+	on_shooting_cooldown = false
