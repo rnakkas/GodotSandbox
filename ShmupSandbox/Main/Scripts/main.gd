23f74892c71fca3992fed53715ad80e61c9b0272
rnakkas <rahman.nakkas@gmail.com>
2025-03-30 15:45:31 +1100

    added enemy 2

---
+++ ShmupSandbox/Main/Scripts/main.gd
@@ -0,0 +1,9 @@
+class_name main extends Node2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	
+	if Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
