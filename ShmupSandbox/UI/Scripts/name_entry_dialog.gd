1a759fd63fbcf2c5d0fc25bf26471594cc34d43d
rnakkas <rahman.nakkas@gmail.com>
2025-04-30 21:36:48 +1000

    Name entry dialog: Added logic for scrolling though letters. See TODO for next work

--- ShmupSandbox/UI/Scripts/name_entry_dialog.gd
+++ ShmupSandbox/UI/Scripts/name_entry_dialog.gd
@@ -3,34 +3,62 @@ class_name NameEntryDialog extends Control
 @onready var score_label : Label = %score_label
 @onready var blink_timer : Timer = $blink_timer
 
-var ui_elements_list : Array[Control] = []
+var letter_labels_list : Array[Label] = []
+var letter_containers_list : Array[Control] = []
 const blink_time : float = 0.4
+# const allowed_chars : Array[String] = [
+# 	"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+# 	"M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X",
+# 	"Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+# 	"!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_",
+# 	"+", "=", "/", "<", ">", "?", ".", ","
+# ]
+
+## FIXME: For testing only, remove
+const allowed_chars : Array[String] = [
+	"A", "B", "C", "D", "E", "F"
+]
+
+var current_char : String 
+
 
 ## TODO: Name entry dialog WIP
-## Try:
-	# Getting the container to be the highlighted element
-	# Then just toggling visibility of the letter label?
+## NEXT:
+	# Being able to confirm on selecting a letter for the name
+	# Once a selection on a letter is made, move focus to the next letter
+	# Once all letters have been selected, send signal to move player to game over screen
+	# Also add a timer to automatically set name and move to game over screen if player idles too long on this dialog
+	# Grab focus on first letter from left when dialog becomes visible, i.e. connect to visibility_changed signal
 
 func _ready() -> void:
 	_initialize_letters_list()
+	_initialize_letter_containers_list()
 	_set_blink_timer_properties()
 
-	## FIXME: Just using this for testing only, will remove
-	ui_elements_list[2]. grab_focus()
+	## FIXME: For testing only, will remove
+	letter_containers_list[2]. grab_focus()
 
 
 ## Get the list of letters and connect to their signals
 func _initialize_letters_list() -> void:
 	for node : Node in get_tree().get_nodes_in_group("name_entry_dialog_letters"):
 		if node is Label:
-			ui_elements_list.append(node)
+			letter_labels_list.append(node)
 			_connect_to_group_signals(node)
-	ui_elements_list.sort()
+	letter_labels_list.sort()
 
-## Helper function for connecting to group signals for the letters
-func _connect_to_group_signals(node : Label) -> void:
+## Get the list of letter containers and connect to their signals
+func _initialize_letter_containers_list() -> void:
+	for node : Node in get_tree().get_nodes_in_group("name_entry_dialog_letter_containers"):
+		if node is Control:
+			letter_containers_list.append(node)
+			_connect_to_group_signals(node)
+	letter_containers_list.sort()
+
+## Helper function for connecting to group signals for the letters and containers
+func _connect_to_group_signals(node : Control) -> void:
 	if node.has_signal("focus_entered"):
-		node.focus_entered.connect(_on_letter_label_focus_entered)
+		node.focus_entered.connect(_on_focus_entered)
 
 
 ## Blinking timer properties
@@ -39,25 +67,55 @@ func _set_blink_timer_properties() -> void:
 	blink_timer.one_shot = false
 
 
-## Helper function for blinking the currently highlighted letter label
-func _blink_current_letter() -> void:
-	for element : int in range(ui_elements_list.size()):
-		if ui_elements_list[element].has_focus():
-			ui_elements_list[element].visible = !ui_elements_list[element].visible
-		elif !ui_elements_list[element].visible:
-			ui_elements_list[element].visible = !ui_elements_list[element].visible
-			ui_elements_list[element].grab_focus()
+## Selecting letters for name entry
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("move_up"):
+		_scroll_up()
+	elif event.is_action_pressed("move_down"):
+		_scroll_down()
+
+
+## When user scrolls up
+func _scroll_up() -> void:
+	for element : int in range(letter_containers_list.size()):
+		if letter_containers_list[element].has_focus():
+			for character : int in range(allowed_chars.size()):
+				if letter_labels_list[element].text == allowed_chars[character]:
+					# Reset char index back to 0 by getting modulus of character+1 vs size of chars array
+					# This loops array back to start
+					character = (character + 1)%allowed_chars.size()
+					letter_labels_list[element].text = allowed_chars[character]
+					break
+
+## When user scrolls down
+func _scroll_down() -> void:
+	for element : int in range(letter_containers_list.size()):
+		if letter_containers_list[element].has_focus():
+			for character : int in range(allowed_chars.size()):
+				if letter_labels_list[element].text == allowed_chars[character]:
+					# Reset char index back to 0 by getting modulus of character-1 vs size of chars array
+					# This loops array back to start
+					character = (character - 1)%allowed_chars.size()
+					letter_labels_list[element].text = allowed_chars[character]
+					break
 
 ####
 
 ## Signals connections
 
-func _on_letter_label_focus_entered() -> void:
-	for element : int in range(ui_elements_list.size()):
-		if ui_elements_list[element].has_focus():
-			UiUtility.highlight_selected_element(ui_elements_list, ui_elements_list[element])
-			blink_timer.start()
+func _on_focus_entered() -> void:
+	for element : int in range(letter_containers_list.size()):
+		if letter_containers_list[element].has_focus():
+			letter_containers_list[element].modulate = UiUtility.color_yellow
+			blink_timer.start()	
+
 
 
 func _on_blink_timer_timeout() -> void:
 	_blink_current_letter()
+
+## Helper function for blinking the currently highlighted letter label
+func _blink_current_letter() -> void:
+	for element : int in range(letter_containers_list.size()):
+		if letter_containers_list[element].has_focus():
+			letter_containers_list[element].get_child(0).visible = !letter_containers_list[element].get_child(0).visible
