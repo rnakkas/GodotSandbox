7b5b517dfff7c67a0d24b4a761e1781e5d757b67
rnakkas <rahman.nakkas@gmail.com>
2025-05-14 07:08:06 +1000

    Audio settings: Continuing ui script

--- ShmupSandbox/UI/Scripts/audio_settings.gd
+++ ShmupSandbox/UI/Scripts/audio_settings.gd
@@ -28,6 +28,10 @@ func _connect_to_group_signals(node : Control) -> void:
 		node.mouse_entered.connect(_on_element_focused_with_mouse.bind(node)) # Use 'bind' to pass source node as property
 	if node is Button || node is TextureButton:
 		node.pressed.connect(_on_button_pressed.bind(node))
+	if node is HSlider:
+		node.drag_started.connect(_on_volume_slider_drag_started)
+		node.drag_ended.connect(_on_volume_slider_drag_ended)
+		node.value_changed.connect(_on_volume_slider_value_changed)
 
 ####
 
@@ -45,16 +49,15 @@ func _on_element_focused() -> void:
 				UiUtility.highlight_selected_element(ui_elements_list, back_button)
 
 
-
 func _on_element_focused_with_mouse(node : Control) -> void:
+	node.grab_focus()
 	if node == master_volume_slider:
 		UiUtility.highlight_selected_element(ui_elements_list, master_label)
 	elif node == sound_volume_slider:
 		UiUtility.highlight_selected_element(ui_elements_list, sound_label)
 	elif node == music_volume_slider:
 		UiUtility.highlight_selected_element(ui_elements_list, music_label)
-	elif node == back_button:
-		node.grab_focus()
+
 
 func _on_button_pressed(node: Control) -> void:
 	if node == back_button:
@@ -65,6 +68,17 @@ func _on_button_pressed(node: Control) -> void:
 		# _update_game_manager()
 		# _save_game_settings()
 
+func _on_volume_slider_drag_started() -> void:
+	print("started dragging")
+
+func _on_volume_slider_drag_ended(value_changed:bool) -> void:
+	if value_changed || !value_changed:
+		print("drag ended")
+
+func _on_volume_slider_value_changed(value: float) -> void:
+	value = int(value)
+	if master_volume_slider.has_focus():
+		print("master volume: ", value)
 
 func _on_visibility_changed() -> void:
 	if self.visible:
