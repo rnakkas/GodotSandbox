d1561e541fc1baeba4a625ddb0c7fbaf9507e2d0
rnakkas <rahman.nakkas@gmail.com>
2025-05-26 20:28:52 +1000

    pickups-powerups: moved shooting logic to dedicated shooting handler

--- ShmupSandbox/Player/Scripts/shooting_handler.gd
+++ ShmupSandbox/Player/Scripts/shooting_handler.gd
@@ -2,12 +2,8 @@ class_name ShootingHandler extends Node2D
 
 #TODO: Will handle all the shooting logic for the player
 ## - Needs the following:
-	# - death state, player script will update it here
 	# - muzzles - for base, od and ch shooting
-	# - sprites for the body and rocket to play and sync shooting and idle animations 
-	#		- maybe better to send signal to parent player node and let the parent handle the animations
-	# - muzzle flash sprites - for base, od and ch
-	#		- see previous point, maybe just send signal to player node
+	# - sends signals to parent player node for shooting and idle animations
 	# - fire rate
 	# - bullet scenes - for base, od and ch
 	# - power up level
@@ -15,3 +11,35 @@ class_name ShootingHandler extends Node2D
 	# - od bullets per shot
 	# - global signal connection - powerup type that was picked up
 	# - emit global signal/event - to add bullets to the game when shooting
+
+@export var fire_rate : float = 8.0
+@export var bullet_scene : PackedScene = preload("res://ShmupSandbox/Player/Scenes/player_bullet.tscn")
+
+@onready var muzzle : Marker2D = $muzzle
+
+signal now_shooting()
+signal stopped_shooting()
+
+var is_dead: bool
+var on_shooting_cooldown : bool
+var shooting_cooldown_time : float
+
+func _ready() -> void:
+	shooting_cooldown_time = 1/fire_rate
+
+func _process(_delta: float) -> void:
+	_handle_shooting()
+
+func _handle_shooting() -> void:
+	if Input.is_action_pressed("shoot"):
+		if !on_shooting_cooldown:
+			on_shooting_cooldown = true
+			
+			var locations : Array[Vector2] = [muzzle.global_position]
+			now_shooting.emit()
+			SignalsBus.player_shooting_event(bullet_scene, locations)
+			
+			await get_tree().create_timer(shooting_cooldown_time).timeout
+			on_shooting_cooldown = false
+	else:
+		stopped_shooting.emit()
