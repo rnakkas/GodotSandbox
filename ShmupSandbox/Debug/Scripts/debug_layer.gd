b9d6d1e0d47c304a94a620c90d435af309d773a9
rnakkas <rahman.nakkas@gmail.com>
2025-06-09 10:31:57 +1000

    pickups-bombs: added previously unstaged changes from last commit

--- ShmupSandbox/Debug/Scripts/debug_layer.gd
+++ ShmupSandbox/Debug/Scripts/debug_layer.gd
@@ -16,9 +16,17 @@ func _input(_event: InputEvent) -> void:
 	
 	if Input.is_key_label_pressed(KEY_Q): # quit game
 		get_tree().quit()
+	
 	if Input.is_key_label_pressed(KEY_R): # reset game
 		get_tree().paused = false
 		get_tree().reload_current_scene()
+	
 	if Input.is_key_label_pressed(KEY_1): # Spawn a powerup on mouse position
 		var mouse_pos : Vector2 = get_viewport().get_mouse_position()
 		SignalsBus.spawn_powerup_event.emit(mouse_pos)
+	
+	if Input.is_key_label_pressed(KEY_2): # Add 1 bomb to stock
+		if GameManager.player_bombs == GameManager.player_max_bombs:
+			return
+		GameManager.player_bombs += 1
+		SignalsBus.player_bombs_updated_event.emit()
