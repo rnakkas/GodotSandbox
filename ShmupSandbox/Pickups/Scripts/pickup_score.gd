8c320979db75e94c881ece17d3c2558888a79974
rnakkas <rahman.nakkas@gmail.com>
2025-06-12 18:00:49 +1000

    pickups-score: Added logic fot collection animation, tweaked powerup font size to be consistent with score item

--- ShmupSandbox/Pickups/Scripts/pickup_score.gd
+++ ShmupSandbox/Pickups/Scripts/pickup_score.gd
@@ -2,9 +2,7 @@ class_name PickupScore extends Area2D
 
 @onready var items_container : Node2D = $sprites_container
 @onready var item_small : AnimatedSprite2D = %sprite_small
-@onready var item_medium : AnimatedSprite2D = %sprite_medium
 @onready var item_large : AnimatedSprite2D = %sprite_large
-@onready var score_label : Label = %score_label
 
 @export var speed : float = 200.0
 
@@ -15,12 +13,12 @@ const score_small : int = 500
 const score_medium : int = 1500
 const score_large : int = 3000
 
-## TODO:
-	# There are 3 varieties: small, medium and large
-	# When spawned, will pick at random from small, medium or large
+## Pickups for score:
+	# There are 2 varieties: small and large
+	# When spawned, will pick at random from small or large
 	# Based on the type, the relevant sprite will be visible, all others will not be visible
 	# Based on the type, score added to player will be different
-	# Each collected item will increase count (in GameManager)
+	# Each collected item will increase count (in GameManager) TODO
 
 ################################################
 #NOTE: Ready and its helper funcs
@@ -28,7 +26,6 @@ const score_large : int = 3000
 func _ready() -> void:
 	_create_items_list()
 	_set_current_item()
-	score_label.visible = false
 
 func _create_items_list() -> void:
 	for node : Node in items_container.get_children():
@@ -75,23 +72,38 @@ func _on_body_entered(body: Node2D) -> void:
 		# Stop moving
 		speed = 0
 
-		# Play collect animations, for both sprites and label
-		# TODO
+		# Play collect animations and tweens, for sprite
+		var tween : Tween = _play_collect_animation()
 
 		# Send global signal that item was picked up
 		SignalsBus.score_increased_event.emit(_item_score())
 
 		# Despawn pickup
-		# TODO: await animations to be finished
+		await current_item.animation_finished
+		await tween.finished
 		call_deferred("queue_free")
+
+
+func _play_collect_animation() -> Tween:
+	# Play collect animation for sprite
+	current_item.play("collect")
+
+	# Tween the sprite when powerup collected
+	var tween = get_tree().create_tween()
+	var final_position : Vector2 = Vector2(current_item.position.x, current_item.position.y - 50.0)
+
+	tween.tween_property(current_item, "position", final_position, 0.2)
+
+	tween.tween_property(current_item, "self_modulate", UiUtility.color_transparent, 0.8) # Fade out effect
+
+	return tween
+
 
 func _item_score() -> int:
 	var score : int
 	match current_item:
 		item_small:
 			score = score_small
-		item_medium:
-			score = score_medium
 		item_large:
 			score = score_large
 	
