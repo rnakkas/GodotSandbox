4a1e8906cf08664b3cf544ea221dd016d634c124
rnakkas <rahman.nakkas@gmail.com>
2025-06-15 06:37:33 +1000

    pickups-score-items: as per new design, started updating the logic for score items, see TODO comment

--- ShmupSandbox/Pickups/Scripts/pickup_score.gd
+++ ShmupSandbox/Pickups/Scripts/pickup_score.gd
@@ -1,24 +1,33 @@
 class_name PickupScore extends Area2D
 
 @onready var items_container : Node2D = $sprites_container
-@onready var item_small : AnimatedSprite2D = %sprite_small
-@onready var item_large : AnimatedSprite2D = %sprite_large
+@onready var item_level_1 : AnimatedSprite2D = %sprite_level_1
+@onready var item_level_2 : AnimatedSprite2D = %sprite_level_2
+@onready var item_level_3 : AnimatedSprite2D = %sprite_level_3
+@onready var item_level_4 : AnimatedSprite2D = %sprite_level_4
+@onready var score_label : Label = %score_label
 
 @export var speed : float = 200.0
 
 var items_list : Array[AnimatedSprite2D] = []
 var current_item : AnimatedSprite2D
 
-const score_small : int = 500
-const score_medium : int = 1500
-const score_large : int = 3000
+const score_level_1 : int = 500
+const score_level_2 : int = 1000
+const score_level_3 : int = 2000
+const score_level_4 : int = 4000
 
-## Pickups for score:
-	# There are 2 varieties: small and large
-	# When spawned, will pick at random from small or large
-	# Based on the type, the relevant sprite will be visible, all others will not be visible
-	# Based on the type, score added to player will be different
-	# Each collected item will increase count (in GameManager) TODO
+
+## TODO Score item pickup:
+	# 4 levels
+	# Always spawns as level 1
+	# When player kills enemy, soul fragments travel to nearest non maxed item and item level +1
+	# If player dies without picking up item, item despawns
+	# Need new spritesheets for level 1-4 spawn, idle, despawn and collect animations
+	# Need scene and sprites for soul fragments
+	# Create a new container in Game scene specifically for score items:
+		# - this should make it easier to find the nearest non max item
+		# - do i need to have an int in pickup_score.gd to track current level?
 
 ################################################
 #NOTE: Ready and its helper funcs
@@ -27,21 +36,25 @@ func _ready() -> void:
 	_create_items_list()
 	_set_current_item()
 
+	# Play spawn animation then play idle
+	current_item.play("spawn")
+	await current_item.animation_finished
+	current_item.play("idle")
+
 func _create_items_list() -> void:
 	for node : Node in items_container.get_children():
 		if node is AnimatedSprite2D:
 			items_list.append(node)
 
 func _set_current_item() -> void:
-	current_item = items_list.pick_random()
+	current_item = item_level_1
 
-	for item : AnimatedSprite2D in items_list:
-		if item == current_item:
-			item.visible = true
-			item.play("idle")
+	for item_sprite : AnimatedSprite2D in items_list:
+		if item_sprite == current_item:
+			item_sprite.visible = true
 		else:
-			item.visible = false
-			item.stop()
+			item_sprite.visible = false
+			item_sprite.stop()
 
 
 ################################################
@@ -72,8 +85,9 @@ func _on_body_entered(body: Node2D) -> void:
 		# Stop moving
 		speed = 0
 
-		# Play collect animations and tweens, for sprite
-		var tween : Tween = _play_collect_animation()
+		# Play collect animations for sprite and label (tween)
+		current_item.play("collect")
+		var tween : Tween = _play_collect_animation_for_label()
 
 		# Send global signal that item was picked up
 		SignalsBus.score_increased_event.emit(_item_score())
@@ -84,17 +98,31 @@ func _on_body_entered(body: Node2D) -> void:
 		call_deferred("queue_free")
 
 
-func _play_collect_animation() -> Tween:
-	# Play collect animation for sprite
-	current_item.play("collect")
+func _play_collect_animation_for_label() -> Tween:
+	# Set label value
+	score_label.text = str(_item_score())
 
-	# Tween the sprite when powerup collected
+	# Tween the label when powerup collected
 	var tween = get_tree().create_tween()
-	var final_position : Vector2 = Vector2(current_item.position.x, current_item.position.y - 50.0)
+	var final_position : Vector2 = Vector2(score_label.position.x, score_label.position.y - 35.0)
 
-	tween.tween_property(current_item, "position", final_position, 0.2)
+	tween.set_parallel(true)
 
-	tween.tween_property(current_item, "self_modulate", UiUtility.color_transparent, 0.8) # Fade out effect
+	tween.tween_property(score_label, "self_modulate", UiUtility.color_yellow, 0.3)
+	tween.tween_property(score_label, "position", final_position, 0.2)
+
+	tween.set_parallel(false)
+
+	# Blinking effect of yellow and white
+	tween.tween_property(score_label, "self_modulate", UiUtility.color_white, 0.1)
+	tween.tween_property(score_label, "self_modulate", UiUtility.color_yellow, 0.1)
+	tween.tween_property(score_label, "self_modulate", UiUtility.color_white, 0.1)
+	tween.tween_property(score_label, "self_modulate", UiUtility.color_yellow, 0.1)
+	tween.tween_property(score_label, "self_modulate", UiUtility.color_white, 0.1)
+	tween.tween_property(score_label, "self_modulate", UiUtility.color_yellow, 0.1)
+
+ 	# Fade out effect
+	tween.tween_property(score_label, "self_modulate", UiUtility.color_transparent, 0.5)
 
 	return tween
 
@@ -102,9 +130,13 @@ func _play_collect_animation() -> Tween:
 func _item_score() -> int:
 	var score : int
 	match current_item:
-		item_small:
-			score = score_small
-		item_large:
-			score = score_large
+		item_level_1:
+			score = score_level_1
+		item_level_2:
+			score = score_level_2
+		item_level_3:
+			score = score_level_3
+		item_level_4:
+			score = score_level_4
 	
 	return score
