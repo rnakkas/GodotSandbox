06ce0aeac178a5b48c54ae959517ae95eecdc0f7
rnakkas <rahman.nakkas@gmail.com>
2025-06-24 06:01:15 +1000

    screamer: started adding screamer enemy

---
+++ ShmupSandbox/Enemies/Scripts/screamer_var_1.gd
@@ -0,0 +1,7 @@
+class_name ScreamerVar1 extends Area2D
+
+@onready var sprite : AnimatedSprite2D = $sprite
+@onready var particles : CPUParticles2D = $CPUParticles2D
+
+@export var speed : float = 210.0
+@export var kill_score : int = 100
